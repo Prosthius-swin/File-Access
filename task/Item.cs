dbb038e90de7026ce10c15e496d1097d0b935818
Callum Hopkins <103919468@student.swin.edu.au>
2022-05-10 21:51:07 +1000

    Completed all cases, successfully loads in list

--- task/Item.cs
+++ task/Item.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace task
 {
@@ -12,7 +7,6 @@ namespace task
         public string title { get; set; }
         public int quantity { get; set; }
         public double unitPrice { get; set; }
-        public List<Item> shoppingList = new List<Item>();
 
         public Item(string pTitle, int pQuantity, double pUnitPrice)
         {
@@ -21,16 +15,4 @@ namespace task
             unitPrice = pUnitPrice;    
         }    
     }
-    class File
-    {
-
-    }
-    /*public class StreamWriter : System.IO.TextWriter
-    {
-        public string filePath { get; set; }
-        public StreamWriter(string pFilePath)
-        {
-            filePath = pFilePath;
-        }
-    }*/
 }
--- task/Program.cs
+++ task/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace task
 {
@@ -11,7 +12,31 @@ namespace task
             string menuChoice = "";
             List<Item> shoppingList = new List<Item>();
             List<double> unitPriceSum = new List<double>();
-            while(true)
+
+            int a = 0;
+            int b = 1;
+            int c = 2;
+            string title;
+            int quantity;
+            double unitPrice;
+
+            string[] lines = File.ReadAllLines("./data.csv");
+            string join = string.Join(",", lines);
+            var values = join.Split(',');
+            foreach (string item in lines)
+            {
+                title = values[a];
+                quantity = int.Parse(values[b]);
+                unitPrice = double.Parse(values[c]);
+
+                shoppingList.Add(new Item(title,quantity,unitPrice));
+
+                a += 3;
+                b += 3;
+                c += 3;
+            }
+
+            while(menuChoice != "6")
             {
                 Console.Write("1. Add New Item \n2. List All Items \n3. Show Total Cost \n4. Clear List \n5. Save List \n6. Exit \n\n");
                 menuChoice = Console.ReadLine();
@@ -21,28 +46,29 @@ namespace task
                 {
                     case "1":
                         Console.Write("Enter item name : ");
-                        string title = Console.ReadLine();
+                        title = Console.ReadLine();
 
                         Console.Write("Enter quantity : ");
-                        int quantity = int.Parse(Console.ReadLine());
+                        quantity = int.Parse(Console.ReadLine());
 
                         Console.Write("Enter unit price : ");
-                        double unitPrice = int.Parse(Console.ReadLine());
+                        unitPrice = int.Parse(Console.ReadLine());
 
-                        shoppingList.Add(new Item(title, quantity, unitPrice));
-                        Console.Write("\nPress any key to return to the main menu\n\n");
-                        Console.ReadKey();
-
-                    break;
+                        shoppingList.Add(new Item(title,quantity,unitPrice));
+                        Console.Write("\nPress 'Enter' to return to the main menu\n------------------------\n\n");
+                        while (Console.ReadKey().Key != ConsoleKey.Enter){}
+                        break;
 
                     case "2":
                         int counter = 1;
                         foreach(Item i in shoppingList)
-                        {
-                            Console.Write($"Item {counter} : {i.title}, {i.quantity}, {i.unitPrice}\n");
-                            counter++;
-                        }
-                        Console.WriteLine();
+                            {
+                                Console.Write($"Item {counter} : {i.title}, {i.quantity}, {i.unitPrice}\n");
+                                counter++;
+                            }
+                            Console.WriteLine();
+                            Console.Write("Press 'Enter' to return to the main menu\n------------------------\n\n");
+                            while (Console.ReadKey().Key != ConsoleKey.Enter){}
                         break;
                     
                     case "3":
@@ -50,21 +76,33 @@ namespace task
                         {
                             unitPriceSum.Add(i.unitPrice);
                         }
-                        Console.WriteLine($"The total cost is {unitPriceSum.Sum()}");
+                        Console.WriteLine($"The total cost is {unitPriceSum.Sum()}\n");
+                        Console.Write("Press 'Enter' to return to the main menu\n------------------------\n\n");
+                        while (Console.ReadKey().Key != ConsoleKey.Enter){}
+                        break;
+
+                    case "4":
+                        shoppingList.Clear();
+                        break;
+
+                    case "5":
+                        
+                        using (StreamWriter writer = new StreamWriter("./data.csv"))  
+                        {  
+                            //writer.Write("Name, Quantity, Price\n\n");
+                            foreach(Item i in shoppingList)
+                            {
+                                writer.Write($"{i.title}, {i.quantity}, {i.unitPrice}\n");
+                            }
+                            Console.WriteLine("Shopping list saved succesfully\n");
+                        }
                         break;
 
                     case "6":
+                        Console.WriteLine("Thank you for using the Shopping List App");
                         break;
                 }
             }
         }
-        /*static public List<Item> Load()
-        {
-            List<Item> items = new List<Item>();
-            if(File.Exists("./data.csv"))
-            {
-                string[] lines = File
-            }
-        }*/
     }
 }
