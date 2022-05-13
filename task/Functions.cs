06a029ba1bb0410dec1bd181d6d5cca5e6debee3
Callum Hopkins <103919468@student.swin.edu.au>
2022-05-13 18:39:43 +1000

    Created functions printHorizontalLine(), saveToCurrentFile()

--- task/Functions.cs
+++ task/Functions.cs
@@ -27,11 +27,13 @@ namespace task
                 listSelection = Console.ReadLine();
             }
         }
+        //
         public static void postMenuSelection()
         {
             Console.Write("Press 'Enter' to return to the main menu\n------------------------\n\n");
             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
+        //
         public static void saveToNewFile(List<Item> shoppingList)
         {
             Console.Write("Enter shopping list name : ");
@@ -45,6 +47,42 @@ namespace task
                     writer.Write($"{i.title}, {i.quantity}, {i.unitPrice}\n");
                 }
                 Console.WriteLine("Shopping list created succesfully\n");
+            }
+        }
+        //
+        public static void printHorizontalLine()
+        {
+            Console.WriteLine("------------------------\n");
+        }
+        //
+        public static void saveToCurrentFile(List<string> savedShoppingListFileName, int listSelectionInt, List<Item> shoppingList, ref string title, ref int quantity, ref double unitPrice)
+        {
+            string[] savedListArr = File.ReadAllLines($"./shopping-lists/{savedShoppingListFileName[listSelectionInt]}");
+            string savedListVar = string.Join(",", savedListArr);
+            var values = savedListVar.Split(',');
+
+            //Used to skip first two lines in .csv to prevent adding them to shoppingList
+            int skip = 1;
+            int a = 4;
+            int b = 5;
+            int c = 6;
+            foreach (var item in savedListArr)
+            {
+                //To prevent adding in headings and blank line
+                if (skip <= 2)
+                {
+                    skip++;
+                    continue;
+                }
+
+                title = values[a].ToString();
+                quantity = int.Parse(values[b]);
+                unitPrice = double.Parse(values[c]);
+                shoppingList.Add(new Item(title, quantity, unitPrice));
+
+                a += 3;
+                b += 3;
+                c += 3;
             }
         }
     }
